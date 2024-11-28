4a9be3e6dd535e07f3357c72f8f098afdb5c2f6c
Philipp Dmitrov <darthgelum@gmail.com>
2024-11-28 23:48:13 +0100

    Yanky added

--- lua/community.lua
+++ lua/community.lua
@@ -7,7 +7,9 @@ return {
   "AstroNvim/astrocommunity",
   -- { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.project.projectmgr-nvim" },
+  { import = "astrocommunity.programming-language-support.rest-nvim", lazy = false },
   { import = "astrocommunity.colorscheme.kanagawa-nvim", lazy = false },
+  { import = "astrocommunity.colorscheme.rose-pine", lazy = false },
   { import = "astrocommunity.file-explorer.telescope-file-browser-nvim", lazy = false },
   -- import/override with your plugins folder
 }
